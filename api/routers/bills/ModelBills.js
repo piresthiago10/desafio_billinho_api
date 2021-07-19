bb6c3a06e8c02a31babddadade94ee791de6b5b4
piresthiago10 <thiagopires.as@gmail.com>
2021-07-19 01:10:17 -0300

    bugfix - comentado codigo relevante

--- api/routers/bills/ModelBills.js
+++ api/routers/bills/ModelBills.js
@@ -1,25 +1,25 @@
-const Sequelize = require('sequelize')
-const instance = require('../../database')
+// const Sequelize = require('sequelize')
+// const instance = require('../../database')
 
-const columns = {
-    amount: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    due_date: {
-        type: Sequelize.DATEONLY,
-        allowNull: false
-    },
-    status: {
-        type: Sequelize.ENUM('open', 'pending', 'paid'),
-        allowNull: true
-    },
-}
+// const columns = {
+//     amount: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false
+//     },
+//     due_date: {
+//         type: Sequelize.DATEONLY,
+//         allowNull: false
+//     },
+//     status: {
+//         type: Sequelize.ENUM('open', 'pending', 'paid'),
+//         allowNull: true
+//     },
+// }
 
-const options = {
-    freezeTableName: true,
-    tableName: 'bills',
-    timestamp: true
-}
+// const options = {
+//     freezeTableName: true,
+//     tableName: 'bills',
+//     timestamp: true
+// }
 
-module.exports = instance.define('bills', columns, options)
+// module.exports = instance.define('bills', columns, options)
