c87c1e325a203eb49eba844a56e4d62bb76a442c
piresthiago10 <thiagopires.as@gmail.com>
2021-07-19 17:31:41 -0300

    feature - novos metodos adicionados

--- api/routers/enrollments/TableEnrollments.js
+++ api/routers/enrollments/TableEnrollments.js
@@ -1,3 +1,4 @@
+const { readId } = require('../students/TableStudents')
 const Model = require('./ModelEnrollments')
 
 module.exports = {
@@ -5,12 +6,41 @@ module.exports = {
         return Model.findAll({
             where: {
                 student: idStudent
-            }
+            },
+            raw: true
         })
     },
+
     create(data) {
         return Model.create(data)
     },
+
+    async readId(idEnrollment, idStudent) {
+        const read = await Model.findOne({
+            where: {
+                id: idEnrollment,
+                student: idStudent
+            },
+            // retorna como objeto puro do javascript
+            raw: true
+        })
+
+        if (!read) {
+            throw new Error('Matrícula não encontrada!')
+        }
+
+        return read
+    },
+
+    update (enrollmentData, dataToUpdate) {
+        return Model.update(
+            dataToUpdate,
+            {
+                where: enrollmentData
+            }
+        )
+    },
+
     delete(idEnrollment, idStudent) {
         return Model.destroy({
             where: {
