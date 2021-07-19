157d40e7ecbad34d31dadf331cabf5a532039c5a
piresthiago10 <thiagopires.as@gmail.com>
2021-07-19 17:31:16 -0300

    feature - novas dotas adicionadas

--- api/routers/enrollments/index.js
+++ api/routers/enrollments/index.js
@@ -2,11 +2,15 @@ const router = require('express').Router({ mergeParams: true })
 const TableEnrollments = require('./TableEnrollments')
 const Enrollment = require('./Enrollment')
 const { request, response } = require('express')
+const Serializer = require('../../Serializer').EnrollmentSerializer
 
 router.get('/', async (request, response) => {
     const enrollments = await TableEnrollments.listEnrollments(request.student.id)
+    const serializer = new Serializer(
+        response.getHeader('Content-Type')
+    )
     response.send(
-        JSON.stringify(enrollments)
+        serializer.serializer(enrollments)
     )
 })
 
@@ -14,11 +18,16 @@ router.post('/', async (request, response, nextMiddleware) => {
     try {
         const idStudent = request.student.id
         const body = request.body
-        const data = Object.assign({}, body, { student: idStudent})
+        const data = Object.assign({}, body, { student: idStudent })
         const enrollment = new Enrollment(data)
         await enrollment.create()
+        const serializer = new Serializer(
+            response.getHeader('Content-Type')
+        )
         response.status(201)
-        response.send(enrollment)
+        response.send(
+            serializer.serializer(enrollment)
+        )
     } catch (error) {
         nextMiddleware(error)
     }
@@ -35,5 +44,48 @@ router.delete('/:id', async (request, response) => {
     response.status(204)
     response.end()
 })
+
+router.get('/:id', async (request, response, nextMiddleWare) => {
+    try {
+        const data = {
+            id: request.params.id,
+            student: request.student.id
+        }
+
+        const enrollment = new Enrollment(data)
+        await enrollment.read()
+        const serializer = new Serializer(
+            response.getHeader('Content-Type'),
+            ['student', 'createdAt', 'updatedAt']
+        )
+        response.send(
+            serializer.serializer(enrollment)
+        )
+    } catch (error) {
+        nextMiddleWare(error)
+    }
+
+})
+
+router.put('/:id', async (request, response, nextMiddleWare) => {
+    try {
+        const data = Object.assign(
+            {},
+            request.body,
+            {
+                id: request.params.id,
+                student: request.student.id
+            }
+        )
+    
+        const enrollment = new Enrollment(data)
+        await enrollment.update()
+        response.status(204)
+        response.end()
+    } catch (error) {
+        nextMiddleWare(error)
+    }
+})
+
 
 module.exports = router
